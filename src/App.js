81b6fbb151a3aa855a81071493d07c87a0cfa449
Rockey Simmons <codenamerockey@gmail.com>
2019-08-21 13:06:21 -0700

    updated file with npx create-react app and use state import

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  const [teamMember, setTeamMember] = useState([]);
+  return <div className="App">App Component</div>;
+}
+
+export default App;
